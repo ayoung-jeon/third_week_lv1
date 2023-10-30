36d82cc67a9766e3d2331583fae2bafcee79792a
ayoung-jeon <wjsdkdud71@naver.com>
2023-10-30 13:55:04 +0900

    second commit

--- src/main/java/com/sparta/board/controller/BoardController.java
+++ src/main/java/com/sparta/board/controller/BoardController.java
@@ -1,2 +1,39 @@
-package com.sparta.board.controller;public class BoardController {
+package com.sparta.board.controller;
+
+
+import com.sparta.board.dto.BoardRequestDto;
+import com.sparta.board.dto.BoardResponseDto;
+import com.sparta.board.service.BoardService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class BoardController {
+
+    private final BoardService boardService;
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    @PostMapping("/boards") //게시글 작성
+    public BoardResponseDto createBoard(@RequestBody BoardRequestDto requestDto) {
+        return boardService.createBoard(requestDto);
+    }
+
+    @GetMapping("/boards") // 게시글 목록 조회
+    public List<BoardResponseDto> getBoards() {
+        return boardService.getBoards();
+    }
+
+    @PutMapping("/boards/{id}") // 게시글 수정
+    public Long updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto requestDto) {
+        return boardService.updateBoard(id, requestDto);
+    }
+
+    @DeleteMapping("/boards/{id}") // 게시글 삭제
+    public Long deleteBoard(@PathVariable Long id) {
+        return boardService.deleteBoard(id);
+    }
 }
--- src/main/java/com/sparta/board/entity/Board.java
+++ src/main/java/com/sparta/board/entity/Board.java
@@ -1,2 +1,35 @@
-package com.sparta.board.entity;public class Board {
+package com.sparta.board.entity;
+
+
+import com.sparta.board.dto.BoardRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity  // JPA가 관리할 수 있는 Entity 클래스 지정
+@Getter
+@Setter
+@Table(name = "board")  // 매핑할 테이블의 이름을 지정
+@NoArgsConstructor
+public class Board extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "username", nullable = false)
+    private String username;
+    @Column(name = "contents", nullable = false, length = 500)
+    private String contents;
+
+    public Board(BoardRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    public void update(BoardRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
 }
